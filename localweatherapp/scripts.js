8a07cb2488e9939d55467e46f5804f110f029ad3
Chazona <chazona@anotherconsolelog.com>
2016-08-06 12:49:07 -0400

    Remove mixed content (http/https)

--- localweatherapp/scripts.js
+++ localweatherapp/scripts.js
@@ -34,11 +34,12 @@ $(document).ready(function() {
         navigator.geolocation.getCurrentPosition(function (position) {
             userLatitude = position.coords.latitude;
             userLongitude = position.coords.longitude;
-            // Use IP to obtain City, ST, Country
-            $.getJSON("http://ip-api.com/json", function(locData) {
-                var userCity = locData.city;
-                var userState = locData.region;
-                var userCountry = locData.country;
+            // Use Ask GEO to obtain City, ST, Country
+            var geoApi = "AIzaSyBKszVxhS9oS9US75Agin439UJvaPGnkQc";
+            $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + userLatitude "," + userLongitude + "&key=" + geoAPI + "?callback=?", function(results) {
+                var userCity = results.address_components[4][long_name];
+                var userState = results.address_components[6][long_name];
+                var userCountry = results.address_components[7][long-name];
                 // Get the City, ST, Country text and replace them with the user's city and state
                 var elemCity = document.getElementById("city");
                 var elemState = document.getElementById("state");
@@ -122,93 +123,6 @@ $(document).ready(function() {
             });
         });
     } else {
-        // Use user's IP to obtain location
-        $.getJSON("http://ip-api.com/json", function(locData) {
-            var userLatitude = locData.lat;
-            var userLongitude = locData.lon;
-            var userCity = locData.city;
-            var userState = locData.region;
-            var userCountry = locData.country;
-            // Get the City, ST text and replace them with the user's city and state
-            var elemCity = document.getElementById("city");
-            var elemState = document.getElementById("state");
-            var elemCountry = document.getElementById("country");
-            elemCity.innerHTML = userCity;
-            elemState.innerHTML = userState;
-            elemCountry.innerHTML = userCountry;
-            // Call Dark Sky Forcast API and retrieve user's weather JSON
-            var apiKey = "56e678830a6e621a5f38e1b43296e432";
-            var weatherStr = "https://api.forecast.io/forecast/" + apiKey + "/" + userLatitude + "," + userLongitude + "?callback=?";
-            $.getJSON(weatherStr, function(weatherData) {
-                // Store user's temp and description and display them
-                var weatherTemp = weatherData.currently.temperature;
-                var feelsLikeTemp = weatherData.currently.apparentTemperature;
-                var weatherType = weatherData.currently.icon;
-                var weatherDescription = weatherData.currently.summary;
-                var elemTemp = document.getElementById("weatherTemp");
-                var elemFeelsLikeTemp = document.getElementById("feelsLikeTemp");
-                elemTemp.innerHTML = parseInt(weatherTemp);
-                elemFeelsLikeTemp.innerHTML = parseInt(feelsLikeTemp);
-                var elemDesc = document.getElementById("weatherDescrip");
-                elemDesc.innerHTML = weatherDescription;
-                // Set background image based on weather so background can reflect hot/cold
-                var elBody = document.getElementById("appBody");
-                switch (weatherType) {
-                    case "clear-day":
-                        elBody.style.backgroundImage = "url(\"../images/clearday.jpg\")";
-                        break;
-                    case "clear-night":
-                        elBody.style.backgroundImage = "url(\"../images/clearnight.jpg\")";
-                        break;
-                    case "rain":
-                        elBody.style.backgroundImage = "url(\"../images/rain.jpg\")";
-                        break;
-                    case "snow":
-                        elBody.style.backgroundImage = "url(\"../images/snow.jpg\")";
-                        break;
-                    case "sleet":
-                        elBody.style.backgroundImage = "url(\"../images/sleet.jpg\")";
-                        break;
-                    case "wind":
-                        elBody.style.backgroundImage = "url(\"../images/wind.jpg\")";
-                        break;
-                    case "fog":
-                        elBody.style.backgroundImage = "url(\"../images/fog.jpg\")";
-                        break;
-                    case "cloudy":
-                        elBody.style.backgroundImage = "url(\"../images/cloudy.jpg\")";
-                        break;
-                    case "partly-cloudy-day":
-                        elBody.style.backgroundImage = "url(\"../images/partlycloudyday.jpg\")";
-                        break;
-                    case "partly-cloudy-night":
-                        elBody.style.backgroundImage = "url(\"../images/partlycloudynight.jpg\")";
-                        break;
-                };
-                // Toggle fahrenheit/celsius
-                var fahrenheit = true;
-                $("#convertBtn").on("click", function() {
-                    var elemSymbol1 = document.getElementById("degreeSym1");
-                    var elemSymbol2 = document.getElementById("degreeSym2");
-                    if (fahrenheit === true) {
-                        weatherTemp = ((weatherTemp - 32) / 1.8);
-                        elemTemp.innerHTML = parseInt(weatherTemp);
-                        feelsLikeTemp = ((feelsLikeTemp - 32) / 1.8);
-                        elemFeelsLikeTemp.innerHTML = parseInt(feelsLikeTemp);
-                        elemSymbol1.innerHTML = "&#8451";
-                        elemSymbol2.innerHTML = "&#8451";
-                        fahrenheit = false;
-                    } else if (fahrenheit === false) {
-                        weatherTemp = ((weatherTemp * 1.8) + 32);
-                        elemTemp.innerHTML = parseInt(weatherTemp);
-                        feelsLikeTemp = ((feelsLikeTemp * 1.8) + 32);
-                        elemFeelsLikeTemp.innerHTML = parseInt(feelsLikeTemp);
-                        elemSymbol1.innerHTML = "&#8457";
-                        elemSymbol2.innerHTML = "&#8457";
-                        fahrenheit = true;
-                    };
-                });
-            }); 
-        });
+        alert("Geolocation not supported on your browser.");
     };
 });
