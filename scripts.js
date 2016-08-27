ae7bcebbf0b2bc91b651fb10067897f2436778d8
Chazona Baum <chznbaum@gmail.com>
2016-08-27 01:07:24 -0400

    Revert "Refactoring"
    This reverts commit bc9092771089728b26ef026cb9c97691a4162eb2.
    
    Refactoring errors
    
    ^X

--- scripts.js
+++ scripts.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-    scrollSpy();
-    var submitted = false;
-function contactForm(message) {
+    // Scrollspy
+    $('body').scrollspy({target: ".navbar", offset: 50});
+    $("#mynavbar a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+    // Skip to content
+    $(".skip").click(function(event){
+        // strip the leading hash and declare
+        // the content we're skipping to
+        var skipTo="#"+this.href.split('#')[1];
+        // Setting 'tabindex' to -1 takes an element out of normal 
+        // tab flow but allows it to be focused via javascript
+        $(skipTo).attr('tabindex', -1).on('blur focusout', function () {
+            // when focus leaves this element, 
+            // remove the tabindex attribute
+            $(this).removeAttr('tabindex');
+        }).focus(); // focus on the content container
+    });
+    // Contact form
+    var submitted=false;
     $("#contactform").on("submit", function(e) {
         $("#contactform *").fadeOut(2000);
         $("#contactform").prepend("Your message has been sent.");
     });
-};
 });
