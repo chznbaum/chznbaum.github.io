477b042dc14110f3e04172931ed140630113642a
Chazona Baum <chazona@chazonabaum.com>
2016-11-15 21:03:56 -0500

    Updating portfolio scripts

---
+++ index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  
+  $('body').scrollspy({target: ".navbar", offset: 50});
+
+  $("#mynavbar a").on('click', function(event) {
+
+    if (this.hash !== "") {
+          event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } 
+  });
+  
+  $(".skip").click(function(event){
+    // strip the leading hash and declare
+    // the content we're skipping to
+    var skipTo="#"+this.href.split('#')[1];
+    
+    // Setting 'tabindex' to -1 takes an element out of normal 
+    // tab flow but allows it to be focused via javascript
+    $(skipTo).attr('tabindex', -1).on('blur focusout', function () {
+      
+      // when focus leaves this element, 
+      // remove the tabindex attribute
+      $(this).removeAttr('tabindex');
+      
+    }).focus(); // focus on the content container
+  });
+});
